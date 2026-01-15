62ebcac4043110a9f5bb17c91e01a57d11cef116
viethuy123 <huytv@runsystem.net>
2026-01-15 11:13:49 +0700

    change logic project dim

---
+++ airflow/dbt/models/dwh/intermediates/create_project_customer.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+
+SELECT
+    _id as id,
+    "customerId" as customer_id,
+    "customerCompanyId" as customer_company_id,
+    "customerName" as customer_name,
+    status,
+    "isDeleted" as is_deleted,
+    {{ safe_parse_timestamp('"createdAt"') }} as created_time,
+    {{ safe_parse_timestamp('"updatedAt"') }} as updated_time
+FROM {{ source('create', 'stg_create_customers') }}

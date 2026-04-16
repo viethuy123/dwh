7a14c08086a32c9cbcf6a67831d2c1b61dbb8829
viethuy123 <huytv@runsystem.net>
2026-04-16 11:32:34 +0700

    add dim seniority

---
+++ airflow/dbt/models/dwh/dim/dim_seniority.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+
+SELECT 1 AS seniority_group_id, '1.  < 2 tháng'      AS seniority_group, 1 AS sort_order
+UNION ALL
+SELECT 2, '2.  2 – < 6 tháng', 2
+UNION ALL
+SELECT 3, '3.  6 – < 12 tháng', 3
+UNION ALL
+SELECT 4, '4.  1 – < 2 năm',   4
+UNION ALL
+SELECT 5, '5.  2 – < 3 năm',   5
+UNION ALL
+SELECT 6, '6.  3 – < 6 năm',   6
+UNION ALL
+SELECT 7, '7.  >= 6 năm',      7
+ORDER BY sort_order;

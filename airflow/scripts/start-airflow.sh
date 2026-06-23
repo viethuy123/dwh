efc44c988579474a5ed50932329ac52f1e13e185
huytv <huytv@runsystem.net>
2026-06-23 08:32:53 +0700

    change cron

--- airflow/scripts/start-airflow.sh
+++ airflow/scripts/start-airflow.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 PROJECT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$PROJECT_DIR"
-docker compose start
+echo "==== $(date) ====" >> /tmp/airflow_start.log
+
+/usr/bin/docker compose up -d >> /tmp/airflow_start.log 2>&1
+
+sleep 30
+
+/usr/bin/docker compose ps >> /tmp/airflow_start.log 2>&1

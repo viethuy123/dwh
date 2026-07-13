08825f87b68c2d895ffa6f004b8292daaa87a383
huytv <huytv@runsystem.net>
2026-07-13 08:27:51 +0700

    refactor: remove dbt dependency installation tasks from all airflow dags

--- airflow/dags/dag-bridge_data.py
+++ airflow/dags/dag-bridge_data.py
@@ -9,7 +9,7 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.sdk import DAG
 
 from config import DBT_PIPELINES, DEFAULT_ARGS, DEFAULT_CHECK_DAG
-from factories.dbt_factory import create_dbt_deps_task, create_dbt_transformation_task_group
+from factories.dbt_factory import create_dbt_transformation_task_group
 
 
 # Lấy config
@@ -30,7 +30,6 @@ dag = DAG(
 
 with dag:
     start = EmptyOperator(task_id='start')
-    dbt_deps = create_dbt_deps_task(dag)
     transformation_group = create_dbt_transformation_task_group(dag, 'dwh', pipeline_config)
     end = EmptyOperator(
         task_id='end',
@@ -38,4 +37,4 @@ with dag:
         trigger_rule=DEFAULT_CHECK_DAG['trigger_rule'],
     )
 
-    start >> dbt_deps >> transformation_group >> end
+    start >> transformation_group >> end
